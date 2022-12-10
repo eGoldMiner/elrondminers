56b207559d0d7b75a9ecbc38333a67329783c96e
Andokarim <ando.karim@gmail.com>
2022-12-10 12:15:34 +0100

    Allow full screen in game

--- src/views/Game.tsx
+++ src/views/Game.tsx
@@ -4,17 +4,31 @@ import Background from './Background';
 import { getIsLoggedIn } from "@elrondnetwork/dapp-core/utils";
 
 
-export default function Game ({ setWindowMint }: any) {
+export default function Game({ setWindowMint }: any) {
 
-  const isLoggedIn = getIsLoggedIn();
+  const [version, setVersion] = useState('');
+  useEffect(() => {
+    async function fetchStringVersion() {
+      try {
+        const response = await fetch('https://elrondminers.com/version.txt');
+        const text = await response.text();
+        setVersion('https://itch.io/embed-upload/' + text + '?color=333333');
+      } catch (error) {
+        // Handle error
+        console.error(error);
+      }
+    }
+
+    fetchStringVersion();
+  }, []);
 
   return (
     <>
-    <div id='div-game-container'>
-      <div id='div-game'>
-        <iframe src="https://itch.io/embed-upload/6956470?color=333333"><a href="https://kartabble.itch.io/elrondminers">Play Elrondminers on itch.io</a></iframe>
+      <div id='div-game-container'>
+        <div id='div-game'>
+          <iframe src={version} allowFullScreen><a href="https://elrondminers.itch.io/elrond-miners-game">Play Elrondminers game on itch.io</a></iframe>
+        </div>
       </div>
-    </div>
     </>
   )
 
