ea91c86bee86901ed685b5cebd1ed87c81ae922f
eGoldMiner <egldminer@gmail.com>
2023-01-18 21:18:01 +0100

    Gestion if Whitelisted Game

--- src/views/Game.tsx
+++ src/views/Game.tsx
@@ -2,10 +2,14 @@
 import React, { useEffect, useRef, useState, createRef } from 'react';
 import Background from './Background';
 import { getIsLoggedIn } from "@elrondnetwork/dapp-core/utils";
+import { useGetAccountInfo, useGetLoginInfo } from "@elrondnetwork/dapp-core/hooks/account";
+import { wl } from "../../src/WL.json"
 
 
 export default function Game({ setWindowMint }: any) {
 
+  const { account } = useGetAccountInfo();
+  const [isWhitelisted, setIsWhitelisted] = useState(false);
   const [version, setVersion] = useState('');
   useEffect(() => {
     async function fetchStringVersion() {
@@ -22,11 +26,29 @@ export default function Game({ setWindowMint }: any) {
     fetchStringVersion();
   }, []);
 
+  useEffect(() => {
+    console.log(wl)
+    console.log(account.address)
+    setIsWhitelisted(wl.includes(account.address))
+    console.log("Whitelisted : " + isWhitelisted)
+  }, []);
+
   return (
     <>
       <div id='div-game-container'>
         <div id='div-game'>
-          <iframe src={version} allowFullScreen><a href="https://elrondminers.itch.io/elrond-miners-game">Play Elrondminers game on itch.io</a></iframe>
+            {isWhitelisted ?
+            (
+              <iframe src={version} allowFullScreen><a href="https://elrondminers.itch.io/elrond-miners-game">Play Elrondminers game on itch.io</a></iframe>
+            ) : (
+              <>
+                <h1 style={{color: "#fff"}}>You are not Whitelisted !</h1>
+                <h1 style={{color: "#fff"}}>Join our Discord here :</h1>
+                <a href="https://discord.gg/MrBcfZhYwy" target="" className="" style={{margin: "auto, 0", textAlign: 'center'}}>
+                  <img src="images/discord.png" loading="lazy" srcSet="images/discord-p-500.png 500w, images/discord.png 512w" sizes="80px" style={{margin: "auto"}}/>
+                </a>
+              </>
+            )}
         </div>
       </div>
     </>
